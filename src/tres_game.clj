8052a441697b2c1ef8fecd2b0f677da6453e7680
Floyd Hightower <floyd.hightower27@gmail.com>
2021-04-21 06:29:09 -0500

    Adding basic functionality

--- src/tres_game.clj
+++ src/tres_game.clj
@@ -1,19 +1,21 @@
 (ns tres-game)
 
-(defn pow [x n]
-  (reduce * (repeat (int n) (int x))))
+(use '[clojure.string :only (split)])
 
-(defn digits [n]
-  (loop [num n
-         d ()]
-    (if (zero? num)
-      d
-      (recur (quot num 10) (conj d (mod num 10))))))
+(def empty-square {:owner nil :count nil})
 
-(defn armstrong-function [num]
-  (apply + 
-    (let [len (count (str num))]
-      (map #(pow % len) (digits num)))))
+(def board {:a {:owner 1 :count 3} :b empty-square :c empty-square
+            :d empty-square :e empty-square :f empty-square
+            :g empty-square :h empty-square :i {:owner 2 :count 3}})
 
-(defn armstrong? [num]
-  (= (armstrong-function num) num))
+(defn get-moves []
+  (println "Enter your move in the form <start><end><count> (e.g. \"ab3\"):")
+  (let [move (read-line)]
+    {:start (first move) :end (second move) :count (Integer. (second (split move #"[a-h]{2}")))}))
+
+(defn update-board [board team-one-moves team-two-moves]
+  (print board))
+
+(defn play [board]
+  (let [team-one-moves (repeat 3 (get-moves)) team-two-moves (repeat 3 get-moves)]
+    (update-board board team-one-moves team-two-moves)))
