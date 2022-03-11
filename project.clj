0371b973b8110f167bd013f574d152631fa1404e
Floyd Hightower <floyd.hightower27@gmail.com>
2022-03-11 08:18:58 -0500

    Simplifying directory structure for cljs

--- project.clj
+++
@@ -1,4 +0,0 @@
-(defproject tres-game "0.1.0-SNAPSHOT"
-  :description "tres-game: One of the fairest games ever invented."
-  :url "https://github.com/fhightower/tres-game"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
